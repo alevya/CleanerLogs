fea6afb70df66ee0539689561e2839dc3ead272a
Vyaznikov Aleksey <alexvya@gmail.com>
2018-01-19 16:15:13 +0300

    change version framework

---
+++ CleanerLogs/Request/FtpLoader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace CleanerLogs.Request
+{
+  internal class FtpLoader
+  {
+    public string Server { get;}
+
+    public FtpLoader(string server)
+    {
+      Server = server;
+    }
+
+    public void ListFilesAsync(string path)
+    {
+      var uri = GetServerUri(path);
+      var ftpState = new FtpState();
+      var request = (FtpWebRequest) WebRequest.Create(uri);
+      request.Method = WebRequestMethods.Ftp.ListDirectory;
+      request.Credentials = new NetworkCredential("anonymous", "anonymous");
+
+      ftpState.Request = request;
+
+    }
+
+    private Uri GetServerUri(string path)
+    {
+      return new Uri($"ftp://{Server}{'/'}{path}");
+    }
+
+  }
+}
---
+++ CleanerLogs/Request/FtpState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace CleanerLogs.Request
+{
+  internal class FtpState
+  {
+    public FtpState()
+    {
+
+      Wait = new ManualResetEvent(false);
+    }
+
+    public ManualResetEvent Wait { get; set; }
+    public FtpWebRequest Request { get; set; }
+    public Exception OperationException { get; set; }
+    public string Status { get; set; }
+    public string FileName { get; set; }
+  }
+}
