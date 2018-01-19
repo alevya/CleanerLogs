1ab37e33c56bc5912b104a55d450761772af9f15
Vyaznikov Aleksey <alexvya@gmail.com>
2018-01-19 13:05:04 +0300

    commit

--- CleanerLogs/ViewModels/MachineDetailViewModel.cs
+++ CleanerLogs/ViewModels/MachineDetailViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using CleanerLogs.ViewModel;
+﻿using CleanerLogs.ViewModel;
 
 namespace CleanerLogs.ViewModels
 {
