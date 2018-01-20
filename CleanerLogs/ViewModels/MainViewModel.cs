fad6c1d6e1d410abfe4871f17bf40487516c2794
alevya <alexv47@mail.ru>
2018-01-21 02:42:58 +0300

    add trottling to download and delete

--- CleanerLogs/ViewModels/MainViewModel.cs
+++ CleanerLogs/ViewModels/MainViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Configuration;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using CleanerLogs.Commands;
@@ -86,10 +89,47 @@ namespace CleanerLogs.ViewModels
 
     private async void CleanAsync(object obj)
     {
-      var md = MachinesDetails.SingleOrDefault(item => item.IsSelected);
-      if (md == null) return;
+      var listMd = MachinesDetails.Where(item => item.IsSelected).ToList();
 
-      var ftpLoader = new FtpClient.FtpClient(md.Ip);
+      const int CONCURRENCY_LEVEL = 3;
+      var mapTasks = new ConcurrentDictionary<Task, string>();
+      var result = new ConcurrentDictionary<string, bool>();
+      int nextIndex = 0;
+
+      while (nextIndex < CONCURRENCY_LEVEL && nextIndex < listMd.Count)
+      {
+        string ip = listMd.ElementAt(nextIndex).Ip;
+        mapTasks.TryAdd(DownloadAndDeleteAsync(ip), ip);
+        nextIndex++;
+      }
+      while (mapTasks.Count > 0)
+      {
+        try
+        {
+          Task resultTask = await Task.WhenAny(mapTasks.Keys);
+          mapTasks.TryRemove(resultTask, out string ipValue);
+          result.TryAdd(ipValue, resultTask.Status == TaskStatus.RanToCompletion);
+          await resultTask;
+        }
+        catch (Exception exc)
+        {
+          // ignored
+        }
+
+        if(nextIndex >= listMd.Count) continue;
+
+        string ip = listMd.ElementAt(nextIndex).Ip;
+        mapTasks.TryAdd(DownloadAndDeleteAsync(ip), ip);
+        nextIndex++;
+
+      }
+
+      MessageBox.Show("Success");
+    }
+
+    private async Task DownloadAndDeleteAsync(string ip)
+    {
+      var ftpLoader = new FtpClient.FtpClient(ip);
       string pathUSBDisk = @"USBDisk\Foreman7";
       string pathNandFlash = @"NandFlash\Foreman7";
 
@@ -102,7 +142,7 @@ namespace CleanerLogs.ViewModels
       foreach (var fileSrc in listUSBDisk.Where(file => file.EndsWith(".log")))
       {
         var pathSrc = Path.Combine(pathUSBDisk, fileSrc);
-        var pathTrg = Path.Combine(diUSBDisk.FullName,fileSrc);
+        var pathTrg = Path.Combine(diUSBDisk.FullName, fileSrc);
         var result = await ftpLoader.DownloadFileAsync(pathSrc, pathTrg);
         if (result == FtpStatusCode.ClosingData)
         {
@@ -121,7 +161,7 @@ namespace CleanerLogs.ViewModels
           await ftpLoader.DeleteFileAsync(pathSrc);
         }
       }
-      MessageBox.Show("Success");
+
     }
 
     private void SelectAll(object obj)
