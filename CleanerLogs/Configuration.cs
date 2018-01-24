b4a67a74621602f7c833eecaa30eef963d8dbd31
Vyaznikov Aleksey <alexvya@gmail.com>
2018-01-24 14:36:14 +0300

    commit

--- CleanerLogs/Configuration.cs
+++ CleanerLogs/Configuration.cs
@@ -6,148 +6,148 @@ namespace CleanerLogs
 {
   public  class ConfigurationApp
   {
-    private readonly Configuration _cfg = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-    private string _savePath;
-    private bool _removeFromBlocks;
-    private bool _zipped;
+      private readonly Configuration _cfg = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+      private string _savePath;
+      private bool _removeFromBlocks;
+      private bool _zipped;
 
-    public ConfigurationApp()
-    {
+      public ConfigurationApp()
+      {
+      }
+
+      public  string SavePath
+      {
+            get
+            {
+            var cSavePath = _cfg.AppSettings.Settings["SavePath"];
+            return cSavePath != null ? cSavePath.Value : _savePath;
+            }
+            set
+            {
+            var cSavePath = _cfg.AppSettings.Settings["SavePath"];
+            if (cSavePath == null)
+            {
+                _savePath = value;
+                return;
+            }
+
+            cSavePath.Value = value;
+            _cfg.Save();
+            }
+      }
+
+      public  bool RemoveFromBlocks
+      {
+
+            get
+            {
+            var cRemoveFromBlocks = _cfg.AppSettings.Settings["RemoveFromBlocks"];
+            if (cRemoveFromBlocks == null) return _removeFromBlocks;
+            bool.TryParse(cRemoveFromBlocks.Value, out _removeFromBlocks);
+            return _removeFromBlocks;
+            }
+            set
+            {
+            var cRemoveFromBlocks = _cfg.AppSettings.Settings["RemoveFromBlocks"];
+            if (cRemoveFromBlocks == null)
+            {
+                _removeFromBlocks = value;
+                return;
+            }
+            cRemoveFromBlocks.Value = Convert.ToString(value);
+            _cfg.Save();
+            }
+      }
+
+      public bool Zipped
+      {
+            get
+            {
+            var cZipped = _cfg.AppSettings.Settings["Zipped"];
+            if (cZipped == null) return _zipped;
+            bool.TryParse(cZipped.Value, out _zipped);
+            return _zipped;
+            }
+            set
+            {
+            var cZipped = _cfg.AppSettings.Settings["Zipped"];
+            if (cZipped == null)
+            {
+                _zipped = value;
+                return;
+            }
+
+            cZipped.Value = Convert.ToString(value);
+            _cfg.Save();
+            }
+      }
+
+      public  int? RequestTimeout
+      {
+            get
+            {
+            var cRequestTimeout = _cfg.AppSettings.Settings["RequestTimeout"];
+            if (cRequestTimeout == null) return null;
+            int.TryParse(cRequestTimeout.Value, out int requestTimeout);
+            return requestTimeout;
+            }
+      }
+
+      public int? ReadWriteTimeout
+      {
+            get
+            {
+            var cReadWriteTimeout = _cfg.AppSettings.Settings["ReadWriteTimeout"];
+            if (cReadWriteTimeout == null) return null;
+            int.TryParse(cReadWriteTimeout.Value, out int readWriteTimeout);
+            return readWriteTimeout;
+            }
+      }
     }
 
-    public  string SavePath
+    public class MachinesConfigSection : ConfigurationSection
     {
-      get
-      {
-        var cSavePath = _cfg.AppSettings.Settings["SavePath"];
-        return cSavePath != null ? cSavePath.Value : _savePath;
-      }
-      set
-      {
-        var cSavePath = _cfg.AppSettings.Settings["SavePath"];
-        if (cSavePath == null)
-        {
-          _savePath = value;
-          return;
-        }
-
-        cSavePath.Value = value;
-        _cfg.Save();
-      }
-    }
-
-    public  bool RemoveFromBlocks
-    {
-
-      get
-      {
-        var cRemoveFromBlocks = _cfg.AppSettings.Settings["RemoveFromBlocks"];
-        if (cRemoveFromBlocks == null) return _removeFromBlocks;
-        bool.TryParse(cRemoveFromBlocks.Value, out _removeFromBlocks);
-        return _removeFromBlocks;
-      }
-      set
-      {
-        var cRemoveFromBlocks = _cfg.AppSettings.Settings["RemoveFromBlocks"];
-        if (cRemoveFromBlocks == null)
-        {
-          _removeFromBlocks = value;
-          return;
-        }
-        cRemoveFromBlocks.Value = Convert.ToString(value);
-        _cfg.Save();
-      }
-    }
-
-    public  bool Zipped
-    {
-      get
-      {
-        var cZipped = _cfg.AppSettings.Settings["Zipped"];
-        if (cZipped == null) return _zipped;
-        bool.TryParse(cZipped.Value, out _zipped);
-        return _zipped;
-      }
-      set
-      {
-        var cZipped = _cfg.AppSettings.Settings["Zipped"];
-        if (cZipped == null)
-        {
-          _zipped = value;
-          return;
-        }
-
-        cZipped.Value = Convert.ToString(value);
-        _cfg.Save();
-      }
-    }
-
-    public  int? RequestTimeout
-    {
-      get
-      {
-        var cRequestTimeout = _cfg.AppSettings.Settings["RequestTimeout"];
-        if (cRequestTimeout == null) return null;
-        int.TryParse(cRequestTimeout.Value, out int requestTimeout);
-        return requestTimeout;
-      }
-    }
-
-    public int? ReadWriteTimeout
-    {
-      get
-      {
-        var cReadWriteTimeout = _cfg.AppSettings.Settings["ReadWriteTimeout"];
-        if (cReadWriteTimeout == null) return null;
-        int.TryParse(cReadWriteTimeout.Value, out int readWriteTimeout);
-        return readWriteTimeout;
-      }
-    }
-  }
-
-  public class MachinesConfigSection : ConfigurationSection
-  {
 
     [ConfigurationProperty("Machines")]
     public MachinesCollection MachineItems
     {
-      get { return (MachinesCollection) base["Machines"]; }
+        get { return (MachinesCollection) base["Machines"]; }
     }
-  }
+    }
 
-  public class MachineElement : ConfigurationElement
-  {
+    public class MachineElement : ConfigurationElement
+    {
     [ConfigurationProperty("number", DefaultValue = "", IsKey = true, IsRequired = true)]
     public string MachineNumber
     {
-      get { return (string)base["number"]; }
-      set { base["number"] = value; }
+        get { return (string)base["number"]; }
+        set { base["number"] = value; }
     }
 
     [ConfigurationProperty("ip", DefaultValue = "", IsKey = false, IsRequired = false)]
     public string MachineIp
     {
-      get { return (string)base["ip"]; }
-      set { base["ip"] = value; }
+        get { return (string)base["ip"]; }
+        set { base["ip"] = value; }
     }
-  }
+    }
 
-  [ConfigurationCollection(typeof(MachineElement), AddItemName = "Machine")]
-  public class MachinesCollection : ConfigurationElementCollection
-  {
+    [ConfigurationCollection(typeof(MachineElement), AddItemName = "Machine")]
+    public class MachinesCollection : ConfigurationElementCollection
+    {
     protected override ConfigurationElement CreateNewElement()
     {
-      return new MachineElement();
+        return new MachineElement();
     }
 
     protected override object GetElementKey(ConfigurationElement element)
     {
-      return ((MachineElement) element).MachineNumber;
+        return ((MachineElement) element).MachineNumber;
     }
 
     public MachineElement this[int index]
     {
-      get { return (MachineElement) BaseGet(index); }
+        get { return (MachineElement) BaseGet(index); }
     }
-  }
+    }
 }
--- CleanerLogs/ViewModels/MainViewModel.cs
+++ CleanerLogs/ViewModels/MainViewModel.cs
@@ -1,276 +1,245 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
 using System.IO;
 using System.Configuration;
 using System.IO.Packaging;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
-using System.Windows.Threading;
 using CleanerLogs.Commands;
 
 namespace CleanerLogs.ViewModels
 {
-  internal class MainViewModel : BaseViewModel
-  {
-
-    private readonly ConfigurationApp _configurationApp;
-    private const string FILE_EXTENSIONS = ".log";
-    private const string FOREMAN = "Foreman7";
-    private const string USBDISK = "USBDisk";
-    private const string NANDFLASH = "NandFlash";
-    private readonly Func<string> _openFileFunc;
-
-    private bool _cursor;
-
-    public MainViewModel(Func<string> openFileFunc)
+    internal class MainViewModel : BaseViewModel
     {
-      _configurationApp = new ConfigurationApp();
-      _openFileFunc = openFileFunc;
-      FileOpenCommand = new DelegateCommand(FileOpen);
-      CleanCommand = new DelegateCommand(CleanAsync);
-      SelectAllCommand = new DelegateCommand(SelectAll);
-    }
+        private readonly ConfigurationApp _configurationApp;
+        private const string FILE_EXTENSIONS = ".log";
+        private const string FOREMAN = "Foreman7";
+        private const string USBDISK = "USBDisk";
+        private const string NANDFLASH = "NandFlash";
+        private readonly Func<string> _openFileFunc;
+        private readonly IDictionary<string, MachineDetailViewModel> _dictMachineDetails = new Dictionary<string, MachineDetailViewModel>();
 
-    #region Properties
+        private bool _cursor;
 
-    public string SavePath
-    {
-      get
-      {
-        var cSavePath = _configurationApp.SavePath;
-        return string.IsNullOrEmpty(cSavePath) ? Path.GetTempPath() : cSavePath;
-      }
-      set
-      {
-          _configurationApp.SavePath = value;
-        OnPropertyChanged();
-      }
-    }
-
-    public bool RemoveFromBlocks
-    {
-      get { return _configurationApp.RemoveFromBlocks; }
-    }
-
-    public bool Zipped
-    {
-      get { return _configurationApp.Zipped; }
-      set
-      {
-          _configurationApp.Zipped = value;
-        OnPropertyChanged();
-      }
-    }
-
-    public bool Cursor
-    {
-      get { return _cursor; }
-      set
-      {
-        _cursor = value;
-        OnPropertyChanged();
-      }
-    }
-
-    public IProgress<TaskProgress> Progress { get; set; }
-
-    public ObservableCollection<MachineDetailViewModel> MachinesDetails { get; set; }
-    
-
-    #endregion
-
-    #region Command
-    public ICommand FileOpenCommand { get; }
-    public ICommand CleanCommand { get; }
-    public ICommand SelectAllCommand { get; }
-    #endregion
-
-    #region Methods
-
-    public void InitConfig()
-    {
-      var m = ConfigurationManager.GetSection("StartupMachines") as MachinesConfigSection;
-      if (m == null || m.MachineItems.Count == 0)
-      {
-        return;
-      }
-
-      MachinesDetails = new ObservableCollection<MachineDetailViewModel>();
-      foreach (MachineElement item in m.MachineItems)
-      {
-        MachinesDetails.Add(new MachineDetailViewModel(item.MachineNumber, item.MachineIp));
-      }
-
-      Progress = new Progress<TaskProgress>(ReportProgress);
-    }
-
-    private void ReportProgress(TaskProgress progress)
-    {
-      var md = MachinesDetails.SingleOrDefault(item => item.IsSelected && item.Ip == progress.CurrentValue);
-      md.Message = progress.CurrentProgressMessage;
-    }
-
-    private void FileOpen(object obj)
-    {
-        var res = _openFileFunc?.Invoke();
-
-    }
-
-    private async void CleanAsync(object obj)
-    {
-        Cursor = true;
-        var listMd = MachinesDetails.Where(item => item.IsSelected).ToList();
-
-        const int CONCURRENCY_LEVEL = 3;
-        var mapTasks = new Dictionary<Task, string>(); 
-        int nextIndex = 0;
-
-        while (nextIndex < CONCURRENCY_LEVEL && nextIndex < listMd.Count)
+        public MainViewModel(Func<string> openFileFunc)
         {
-        string ip = listMd.ElementAt(nextIndex).Ip;
-        mapTasks.Add(DownloadAndDeleteAsync(ip), ip);
-        nextIndex++;
+            _configurationApp = new ConfigurationApp();
+            _openFileFunc = openFileFunc;
+            FileOpenCommand = new DelegateCommand(FileOpen);
+            CleanCommand = new DelegateCommand(CleanAsync);
+            SelectAllCommand = new DelegateCommand(SelectAll);
         }
-        while (mapTasks.Count > 0)
+
+   #region Properties
+
+        public string SavePath
         {
-        string ipValue = String.Empty;
-        try
-        {
-            Task resultTask = await Task.WhenAny(mapTasks.Keys);
-            mapTasks.TryGetValue(resultTask, out ipValue);
-            mapTasks.Remove(resultTask);
-            await resultTask;
-         
-            Progress.Report(new TaskProgress{CurrentProgress = nextIndex
-                                            ,TotalProgress = listMd.Count
-                                            ,CurrentProgressMessage = $"On {ipValue} ",CurrentValue = ipValue
-            });
-        }
-        catch (Exception exc)
-        {
-            Progress.Report(new TaskProgress
+            get
             {
-            CurrentProgress = nextIndex
-            ,
-            TotalProgress = listMd.Count
-            ,
-            CurrentProgressMessage = exc.Message
-            ,
-            CurrentValue = ipValue
-            });
+            var cSavePath = _configurationApp.SavePath;
+            return string.IsNullOrEmpty(cSavePath) ? Path.GetTempPath() : cSavePath;
+            }
+            set
+            {
+                _configurationApp.SavePath = value;
+            OnPropertyChanged();
+            }
         }
 
-        if(nextIndex >= listMd.Count) continue;
-
-        string ip = listMd.ElementAt(nextIndex).Ip;
-        mapTasks.Add(DownloadAndDeleteAsync(ip), ip);
-        nextIndex++;
+        public bool RemoveFromBlocks
+        {
+            get { return _configurationApp.RemoveFromBlocks; }
         }
 
-        Cursor = false;
-    }
+        public bool Zipped
+        {
+            get { return _configurationApp.Zipped; }
+            set
+            {
+                _configurationApp.Zipped = value;
+            OnPropertyChanged();
+            }
+        }
 
-    private async Task DownloadAndDeleteAsync(string ip)
-    {
-      var ftpLoader = new FtpClient.FtpClient(ip, _configurationApp.RequestTimeout, _configurationApp.ReadWriteTimeout);
+        public bool Cursor
+        {
+            get { return _cursor; }
+            set
+            {
+            _cursor = value;
+            OnPropertyChanged();
+            }
+        }
     
-      var listUsbDisk = await ftpLoader.ListFilesAsync(BuildRemotePath(USBDISK));
-      var listNandFlash = await ftpLoader.ListFilesAsync(BuildRemotePath(NANDFLASH));
+        public ObservableCollection<MachineDetailViewModel> MachinesDetails { get; set; }
+   #endregion
 
-      var logsUsbDisk = listUsbDisk.Where(file => file.EndsWith(FILE_EXTENSIONS));
-      var logsNandDisk = listNandFlash.Where(file => file.EndsWith(FILE_EXTENSIONS));
+   #region Command
+        public ICommand FileOpenCommand { get; }
+        public ICommand CleanCommand { get; }
+        public ICommand SelectAllCommand { get; }
+   #endregion
 
-      var rootPathTrg = Path.Combine(SavePath, ip);
+   #region Methods
 
-      if (Zipped)
-      {
-        using (var package = Package.Open(rootPathTrg + ".zip", FileMode.Create, FileAccess.ReadWrite))
+        public void InitConfig()
         {
-          foreach (var fileSrc in logsUsbDisk)
-          {
-            await DoDownloadZipAsync(fileSrc, package, USBDISK, ftpLoader);
-          }
-          foreach (var fileSrc in logsNandDisk)
-          {
-            await DoDownloadZipAsync(fileSrc, package, NANDFLASH, ftpLoader);
-          }
-        }
-      }
-      else
-      {
-        var diUsbDisk = Directory.CreateDirectory(Path.Combine(rootPathTrg, USBDISK));
-        var diNandFlash = Directory.CreateDirectory(Path.Combine(rootPathTrg, NANDFLASH));
+            var m = ConfigurationManager.GetSection("StartupMachines") as MachinesConfigSection;
+            if (m == null || m.MachineItems.Count == 0)
+            {
+            return;
+            }
 
-        foreach (var fileSrc in logsUsbDisk)
+            foreach (MachineElement item in m.MachineItems)
+            {
+                _dictMachineDetails.Add(item.MachineNumber, new MachineDetailViewModel(item.MachineNumber, item.MachineIp));
+            }
+
+            MachinesDetails = new ObservableCollection<MachineDetailViewModel>(_dictMachineDetails.Values);
+        }
+
+        private void FileOpen(object obj)
         {
-          var pathTrg = Path.Combine(diUsbDisk.FullName, fileSrc);
-          await DoDownloadAsync(fileSrc, pathTrg, USBDISK, ftpLoader);
+            var res = _openFileFunc?.Invoke();
         }
-        foreach (var fileSrc in logsNandDisk)
+
+        private async void CleanAsync(object obj)
         {
-          var pathTrg = Path.Combine(diNandFlash.FullName, fileSrc);
-          await DoDownloadAsync(fileSrc, pathTrg, NANDFLASH, ftpLoader);
+            Cursor = true;
+            var listMd = MachinesDetails.Where(item => item.IsSelected).ToList();
+
+            const int CONCURRENCY_LEVEL = 10;
+            var mapTasks = new Dictionary<Task, string>(); 
+            int nextIndex = 0;
+
+            while (nextIndex < CONCURRENCY_LEVEL && nextIndex < listMd.Count)
+            {
+                var md = listMd.ElementAt(nextIndex);
+                mapTasks.Add(DownloadAndDeleteAsync(md.Ip), md.Number);
+                nextIndex++;
+            }
+            while (mapTasks.Count > 0)
+            {
+                string numberValue = String.Empty;
+                try
+                {
+                    Task resultTask = await Task.WhenAny(mapTasks.Keys);
+                    mapTasks.TryGetValue(resultTask, out numberValue);
+                    mapTasks.Remove(resultTask);
+                    await resultTask;
+
+                    _updateMachineItemMessage(numberValue, "Успешно!");
+                }
+                catch (Exception exc)
+                {
+                    _updateMachineItemMessage(numberValue, exc.Message);
+                }
+
+                if(nextIndex >= listMd.Count) continue;
+
+                var md = listMd.ElementAt(nextIndex);
+                mapTasks.Add(DownloadAndDeleteAsync(md.Ip), md.Number);
+                nextIndex++;
+            }
+
+            Cursor = false;
         }
-      }
-    }
 
-    private async Task DoDownloadAsync(string fileNameSrc, string pathTrg, string nameRootStorage, FtpClient.FtpClient ftpLoader)
-    {
-      var pathSrc = Path.Combine(BuildRemotePath(nameRootStorage), fileNameSrc);
+        private async Task DownloadAndDeleteAsync(string ip)
+        {
+            var ftpLoader = new FtpClient.FtpClient(ip, _configurationApp.RequestTimeout, _configurationApp.ReadWriteTimeout);
+    
+            var listUsbDisk = await ftpLoader.ListFilesAsync(BuildRemotePath(USBDISK));
+            var listNandFlash = await ftpLoader.ListFilesAsync(BuildRemotePath(NANDFLASH));
 
-      var result = await ftpLoader.DownloadFileAsync(pathSrc, pathTrg);
+            var logsUsbDisk = listUsbDisk.Where(file => file.EndsWith(FILE_EXTENSIONS));
+            var logsNandDisk = listNandFlash.Where(file => file.EndsWith(FILE_EXTENSIONS));
 
-      if (result == FtpStatusCode.ClosingData && RemoveFromBlocks)
-      {
-        await ftpLoader.DeleteFileAsync(pathSrc);
-      }
+            var rootPathTrg = Path.Combine(SavePath, ip);
 
-    }
+            if (Zipped)
+            {
+            using (var package = Package.Open(rootPathTrg + ".zip", FileMode.Create, FileAccess.ReadWrite))
+            {
+                foreach (var fileSrc in logsUsbDisk)
+                {
+                await DoDownloadZipAsync(fileSrc, package, USBDISK, ftpLoader);
+                }
+                foreach (var fileSrc in logsNandDisk)
+                {
+                await DoDownloadZipAsync(fileSrc, package, NANDFLASH, ftpLoader);
+                }
+            }
+            }
+            else
+            {
+            var diUsbDisk = Directory.CreateDirectory(Path.Combine(rootPathTrg, USBDISK));
+            var diNandFlash = Directory.CreateDirectory(Path.Combine(rootPathTrg, NANDFLASH));
 
-    private async Task DoDownloadZipAsync(string fileNameSrc, Package zipPackage, string nameRootStorage,  FtpClient.FtpClient ftpLoader)
-    {
-      var pathSrc = Path.Combine(BuildRemotePath(nameRootStorage), fileNameSrc);
+            foreach (var fileSrc in logsUsbDisk)
+            {
+                var pathTrg = Path.Combine(diUsbDisk.FullName, fileSrc);
+                await DoDownloadAsync(fileSrc, pathTrg, USBDISK, ftpLoader);
+            }
+            foreach (var fileSrc in logsNandDisk)
+            {
+                var pathTrg = Path.Combine(diNandFlash.FullName, fileSrc);
+                await DoDownloadAsync(fileSrc, pathTrg, NANDFLASH, ftpLoader);
+            }
+            }
+        }
+
+        private async Task DoDownloadAsync(string fileNameSrc, string pathTrg, string nameRootStorage, FtpClient.FtpClient ftpLoader)
+        {
+            var pathSrc = Path.Combine(BuildRemotePath(nameRootStorage), fileNameSrc);
+
+            var result = await ftpLoader.DownloadFileAsync(pathSrc, pathTrg);
+
+            if (result == FtpStatusCode.ClosingData && RemoveFromBlocks)
+            {
+            await ftpLoader.DeleteFileAsync(pathSrc);
+            }
+        }
+
+        private async Task DoDownloadZipAsync(string fileNameSrc, Package zipPackage, string nameRootStorage,  FtpClient.FtpClient ftpLoader)
+        {
+            var pathSrc = Path.Combine(BuildRemotePath(nameRootStorage), fileNameSrc);
       
-      var partUri = PackUriHelper.CreatePartUri(new Uri(Path.Combine(nameRootStorage, fileNameSrc), UriKind.Relative));
-      var packagePart = zipPackage.CreatePart(partUri, System.Net.Mime.MediaTypeNames.Text.Plain, CompressionOption.Normal);
-      var result = await ftpLoader.DownloadFileAsync(pathSrc, packagePart.GetStream());
+            var partUri = PackUriHelper.CreatePartUri(new Uri(Path.Combine(nameRootStorage, fileNameSrc), UriKind.Relative));
+            var packagePart = zipPackage.CreatePart(partUri, System.Net.Mime.MediaTypeNames.Text.Plain, CompressionOption.Normal);
+            var result = await ftpLoader.DownloadFileAsync(pathSrc, packagePart.GetStream());
      
-      if (result == FtpStatusCode.ClosingData && RemoveFromBlocks)
-      {
-        await ftpLoader.DeleteFileAsync(pathSrc);
-      }
-    }
+            if (result == FtpStatusCode.ClosingData && RemoveFromBlocks)
+            {
+            await ftpLoader.DeleteFileAsync(pathSrc);
+            }
+        }
 
-    private string BuildRemotePath(string nameRootPath)
-    {
-      return Path.Combine(nameRootPath, FOREMAN);
-    }
+        private string BuildRemotePath(string nameRootPath)
+        {
+            return Path.Combine(nameRootPath, FOREMAN);
+        }
 
-    private void SelectAll(object obj)
-    {
-      if(MachinesDetails == null) return;
+        private void SelectAll(object obj)
+        {
+            if(MachinesDetails == null) return;
 
-      var isChecked = (bool) obj;
-      foreach (var item in MachinesDetails)
-      {
-        item.IsSelected = isChecked;
-      }
-    }
-    #endregion
-  }
+            var isChecked = (bool) obj;
+            foreach (var item in MachinesDetails)
+            {
+            item.IsSelected = isChecked;
+            }
+        }
 
-  public class TaskProgress
-  {
-    public int CurrentProgress { get; set; }
-    public int TotalProgress { get; set; }
-    public string CurrentProgressMessage { get; set; }
-    public string CurrentValue { get; set; }
+        private void _updateMachineItemMessage(string number, string message)
+        {
+            _dictMachineDetails.TryGetValue(number, out MachineDetailViewModel md);
+            if (md != null) md.Message = message;
+        }
+   #endregion
   }
 
 }
